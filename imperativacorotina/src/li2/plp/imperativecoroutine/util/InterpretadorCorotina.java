6ef5d5026b9266f58ef5d80eae37a27b79cd2f83
Carlos Zimmerle <cezl@cin.ufpe.br>
2018-04-25 21:04:03 -0300

    Criação de classe para executar a corotina

---
+++ imperativacorotina/src/li2/plp/imperativecoroutine/util/InterpretadorCorotina.java
@@ -0,0 +1,38 @@
+package li2.plp.imperativecoroutine.util;
+
+import li2.plp.imperative1.command.Comando;
+import li2.plp.imperative1.memory.AmbienteExecucaoImperativa;
+
+public class InterpretadorCorotina extends Coroutine{
+	private Comando comando;
+	private AmbienteExecucaoImperativa ambiente;
+	
+	public InterpretadorCorotina(Comando comando, AmbienteExecucaoImperativa ambiente) {
+		this.comando = comando;
+		this.ambiente = ambiente;
+	}
+	
+	protected void body() {
+		
+		try {
+			ambiente = comando.executar(ambiente);
+		}catch (Exception e) {
+			System.out.println("Imperativa 2 PLP Parser Version 0.0.1:  Encountered errors during parse.");
+            e.printStackTrace();
+		}
+		
+	}
+	
+	public Comando getComando() {
+		return comando;
+	}
+	public void setComando(Comando comando) {
+		this.comando = comando;
+	}
+	public AmbienteExecucaoImperativa getAmbiente() {
+		return ambiente;
+	}
+	public void setAmbiente(AmbienteExecucaoImperativa ambiente) {
+		this.ambiente = ambiente;
+	}
+}
